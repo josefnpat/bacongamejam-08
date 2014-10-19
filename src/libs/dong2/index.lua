bc07e0cf171222366f80ad1257eb1061b120fed3
Seppi <seppi@localhost.localdomain>
2014-10-19 11:12:03 -0400

    Adding dong2 and gamestate and lovebird

---
+++ src/libs/dong2/index.lua
@@ -0,0 +1,21 @@
+local index = {
+  XBOX_360 = {
+    "Xbox 360 Wireless Receiver",
+    "Wireless 360 Controller", -- OS X
+  },
+  XBOX_360_XINPUT = {
+    "XInput Controller #1",
+    "XInput Controller #2",
+    "XInput Controller #3",
+    "XInput Controller #4"
+  },
+  OUYA = {"OUYA Game Controller"},
+  PS3 = {
+    "MotioninJoy Virtual Game Controller", -- Windows w/ MotioninJoy
+    "PLAYSTATION(R)3 Controller", -- OUYA
+    "Sony PLAYSTATION(R)3 Controller" -- EVERYONE ELSE
+  },
+  RETROLINK_NES = {"USB Gamepad "}
+}
+
+return index
